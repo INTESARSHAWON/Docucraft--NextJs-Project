8cd0f67f5e5ee5844e587373c752cdfb51dc3329
Intesar Shawon <intesarshawon@gmail.com>
2025-06-29 03:25:16 +0600

    fix initial portion

---
+++ components/Logo.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image"; 
+
+export default function Logo() {
+  return (
+    <>
+    <div className="lg:flex">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              className="h-6 w-auto"
+              width={100}
+              height={24}
+              priority
+            />
+          </Link>
+        </div>
+      
+    </>
+  )
+}
